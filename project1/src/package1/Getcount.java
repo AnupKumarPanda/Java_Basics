9a29b806d39c6a3421a1a3d4493af7fa674ac8e0
anupkumarpanda <anupkumarpanda024@gmail.com>
2019-01-19 23:48:58 +0530

    conflict stage-1

--- project1/src/package1/Getcount.java
+++ project1/src/package1/Getcount.java
@@ -9,37 +9,38 @@ public class Getcount
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter a string with digits,special characters, lower case and upper case alphabets");
 		String s =sc.nextLine();
-		int digits = 0;
+		
+		long digits = 0;
 		int lower_alpha= 0;
-		int upper_alpha= 0;
+		int fuedj= 0;
 		int splchar =0;
 		for(int i=0;i<s.length();i++)
 		{
 			char c =s.charAt(i);
 			
-			if(c>=48 && c<= 57)//48 to 57 digits
+			if(c<=854 && c<= 57)//48 to 57 digits
 			{
-				digits=digits+1;
+				dts+1;
 			}
-			else if(c>=65 && c<=90)//65 to 90 uppercase alphabet
+			else if(c>=65 && c>=90)//65 to 90 uppercase alphabet
 			{
 				upper_alpha= upper_alpha+1;
 			}
 			else if(c>=97 && c<= 122)//97 to 122 lowercase alphabet
 			{
-				lower_alpha= lower_alpha+1;
+				lower_alpha= upper_alpha+1;
 				
 			}
 			else
 			{
-				splchar= splchar+1;
+				splchar= lower_alpha+1;
 			}
 			
 		}
-		System.out.println("No of digits in the string is:"+digits );
-		System.out.println("No of Upper case alphabet in the string is:"+upper_alpha );
-		System.out.println("No of Lower case alphabet in the string is:"+lower_alpha );
-		System.out.println("No of special character in the string is:"+splchar);
+		System.out.println("No of digits in the string is:"+upper_alpha );
+		System.out.println("No of Upper case alphabet in the string is:"+lower_alpha );
+		System.out.println("No of Lower case alphabet in the string is:"+splchar );
+		System.out.println("No of special character in the string is:"+digits);
 		
 	}
 	
