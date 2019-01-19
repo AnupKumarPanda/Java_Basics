f71f926052bd804b98f6d83137a2eadbd70b6226
anupkumarpanda <anupkumarpanda024@gmail.com>
2019-01-19 23:29:16 +0530

    getcount and regular expression program

---
+++ project1/src/package1/Getcount.java
@@ -0,0 +1,47 @@
+package package1;
+
+import java.util.Scanner;
+
+public class Getcount 
+{// get count of digits, Lowercaase alphabets, uppercase alphabets in a given string using ascii code
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a string with digits,special characters, lower case and upper case alphabets");
+		String s =sc.nextLine();
+		int digits = 0;
+		int lower_alpha= 0;
+		int upper_alpha= 0;
+		int splchar =0;
+		for(int i=0;i<s.length();i++)
+		{
+			char c =s.charAt(i);
+			
+			if(c>=48 && c<= 57)//48 to 57 digits
+			{
+				digits=digits+1;
+			}
+			else if(c>=65 && c<=90)//65 to 90 uppercase alphabet
+			{
+				upper_alpha= upper_alpha+1;
+			}
+			else if(c>=97 && c<= 122)//97 to 122 lowercase alphabet
+			{
+				lower_alpha= lower_alpha+1;
+				
+			}
+			else
+			{
+				splchar= splchar+1;
+			}
+			
+		}
+		System.out.println("No of digits in the string is:"+digits );
+		System.out.println("No of Upper case alphabet in the string is:"+upper_alpha );
+		System.out.println("No of Lower case alphabet in the string is:"+lower_alpha );
+		System.out.println("No of special character in the string is:"+splchar);
+		
+	}
+	
+
+}
---
+++ project1/src/package1/RegularExpressions.java
@@ -0,0 +1,12 @@
+package package1;
+
+public class RegularExpressions
+{
+//Pattern matching using Regular Expression
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
